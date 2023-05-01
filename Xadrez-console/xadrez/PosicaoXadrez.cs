dcdef446410b8277aca5d1c6d9cfceed96ed0497
Leonardo Siqueira <leo2017siqueira@outlook.com>
2023-05-01 12:19:54 -0300

    Criando a classe PosicaoXadrez

---
+++ Xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public char linha { get; set; }
+
+        public PosicaoXadrez(char coluna, char linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
